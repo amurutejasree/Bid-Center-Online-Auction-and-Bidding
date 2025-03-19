823bd5ce8821c7e934336d47d484d97a878b5a5e
A TEJA SREE <137806889+amurutejasree@users.noreply.github.com>
2025-03-19 16:30:23 +0530

    Delete bid-center online directory

--- bid-center online/src/main/java/com/example/AuctionBazaar/repository/AuctionImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.AuctionBazaar.repository;
-
-import com.example.AuctionBazaar.Model.AuctionImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuctionImageRepository extends JpaRepository<AuctionImage, Long> {}
--- bid-center online/src/main/java/com/example/AuctionBazaar/repository/AuctionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.AuctionBazaar.repository;
-
-import com.example.AuctionBazaar.Model.Auction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuctionRepository extends JpaRepository<Auction, Long> {}
